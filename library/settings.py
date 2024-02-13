ecb64710d52bc15bc9f29c46cb21680c9f31abdc
Jasur Kenjayev <Jokerqrol@gmail.com>
2024-02-13 10:53:26 +0500

    setting

--- library/settings.py
+++ library/settings.py
@@ -64,9 +64,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    #download modelver
-    'allauth.account.middleware.AccountMiddleware',
 ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
